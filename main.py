53f2e124604f41ba72ac46c65b1e2970845bcce4
Sven Weidenmann <sweidenmann@gmail.com>
2024-02-17 13:08:23 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def reverse_linked_list(head):
+    prev = None
+    current = head
+    while current is not None:
+        next_node = current.next
+        current.next = prev
+        prev = current
+        current = next_node
+    return prev
+
+# linked list: 1 -> 2 -> 3 -> 4 -> 5
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(3)
+head.next.next.next = ListNode(4)
+head.next.next.next.next = ListNode(5)
+
+# Reverse the linked list
+new_head = reverse_linked_list(head)
+
+# Print the reversed linked list
+while new_head is not None:
+    print(new_head.val, end=" ")
+    new_head = new_head.next
